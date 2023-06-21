a2bd7363fef84b4195b286e43a61d21aa7673a9e
KhangOE <19521663@gm.uit.edu.vn>
2023-06-21 16:26:03 +0700

    add top ten

--- Repository/OrderRepository.cs
+++ Repository/OrderRepository.cs
@@ -12,28 +12,28 @@ namespace Product_management.Repository
 
         public OrderRepository(DataContext dataContext)
         {
-                _dataContext = dataContext;
+            _dataContext = dataContext;
         }
         public async Task<ICollection<Order>> GetAll()
         {
             return await _dataContext.Orders
                 .Include(x => x.OrderDetails)
                 .ThenInclude(x => x.Product)
-                .Include(x => x.User).ToListAsync();    
+                .Include(x => x.User).ToListAsync();
         }
 
         // user order nhiều nhất tháng
         public async Task<User> HighestOrderedUser()
         {
             var currentTime = DateTime.Now;
-            var users =  _dataContext.Users.ToList();
-     
+            var users = _dataContext.Users.ToList();
+
             // user has highest number order this month lambda
             var HighestOrderUser = _dataContext.Users.ToList()
                    .MaxBy(x => x.Orders.Count);
 
-           // var maxValue = 
-            var query3 =  (from user in users
+            // var maxValue = 
+            var query3 = (from user in users
                           join order in (from order in _dataContext.Orders
                                          where order.CreateDate.Month == currentTime.Month
                                            && order.CreateDate.Year == currentTime.Year
@@ -41,10 +41,28 @@ namespace Product_management.Repository
                           on user.Id equals order.UserId into userGroup
                           where userGroup.Count() == users.Max(x => x.Orders.Count)
                           select user).FirstOrDefault();
-            return  query3;
+            return query3;
         }
 
-     
+        public async Task<List<Product>> TopTenBoughProduct(){
+
+            var products = _dataContext.Products.ToList();
+            var orderdetails = _dataContext.OrderDetails.ToList();
+
+            var lambda = products.OrderByDescending(p => p.OrderDetails
+                                    .Sum(x => x.quantity))
+                                 .ToList()
+                                 .Take(10);
+
+            var query = (from product in products
+                         join orderDetail in orderdetails 
+                         on product.Id equals orderDetail.ProductId into productGroup
+                         orderby productGroup.Sum(x => x.quantity) descending
+                         select product).Take(10).ToList();
+
+            return query;
+
+        }
 
         //sản phẩm được mua nhiều nhất tháng
         public async Task<Product> HighestBoughProduct()
